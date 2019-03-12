14142141078fc41ec3b756edc1ee8158e0ea794a
Sándor Gazdag <sandor.gazdag@iov42.com>
2019-03-12 18:38:34 +0100

    Initial Commit

---
+++ tapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tapp
+//
+//  Created by Gazdag Sándor on 2019. 03. 12..
+//  Copyright © 2019. Gazdag Sándor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
